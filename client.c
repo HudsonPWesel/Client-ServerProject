a94a55429131d3cc7f3360ef1900d5034a992393
HudsonPWesel <cswyld7k@gmail.com>
2024-12-02 12:54:32 -0500

    Update IP

--- client.c
+++ client.c
@@ -4,9 +4,9 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define SERVER_IP "127.0.0.1" // The server's IP address (use Raspberry Pi server IP in actual setup)
-#define SERVER_PORT 9999      // Port the server is listening on
-#define BUFFER_SIZE 1024      // Buffer size for messages
+#define SERVER_IP "10.33.96.6" // The server's IP address (use Raspberry Pi server IP in actual setup)
+#define SERVER_PORT 9999       // Port the server is listening on
+#define BUFFER_SIZE 1024       // Buffer size for messages
 #define MAX_LINES 100
 #define MAX_LEN 100
 
